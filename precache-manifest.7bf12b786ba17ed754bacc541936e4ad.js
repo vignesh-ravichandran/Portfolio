6f1f4dc40710aa2b5ddac25e71eee384fbc10d88
Vignesh Ravichandran <vigneshravichanders@gmail.com>
2020-03-11 21:56:31 +0530

    Updates

---
+++ precache-manifest.7bf12b786ba17ed754bacc541936e4ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24b0c420365fe61e16822bfe2e519b6",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "9938e1f48ba2addfbb52",
+    "url": "/Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6f9329585204114f249a",
+    "url": "/Portfolio/static/js/2.f54247d6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Portfolio/static/js/2.f54247d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9938e1f48ba2addfbb52",
+    "url": "/Portfolio/static/js/main.26faa789.chunk.js"
+  },
+  {
+    "revision": "b56498811c90b2f2740c",
+    "url": "/Portfolio/static/js/runtime-main.d38ab006.js"
+  }
+]);
